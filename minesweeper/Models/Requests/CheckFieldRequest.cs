540f13518950d20fea43ca848bb024ded1020806
PartyMar <60110864+PartyMar@users.noreply.github.com>
2024-05-20 00:57:34 +0300

    re-init project

---
+++ minesweeper/Models/Requests/CheckFieldRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace minesweeper.Models.Requests
+{
+    public class CheckFieldRequest
+    {
+       // public string Game_id { get; set; }
+        public int Col { get; set; }
+        public int Row { get; set; }
+    }
+}
---
+++ minesweeper/Models/Requests/NewGameRequest.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace minesweeper.Models.Requests
+
+{
+    public class NewGameRequest
+    {
+        [Required] public int Width { get; set; }
+        [Required] public int Height { get; set; }
+        [Required] public int Mines_count { get; set; }
+    }
+}
